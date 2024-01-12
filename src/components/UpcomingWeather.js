a6cf8b3bebf91278d11454cca2affcfd5a0cf7c7
asb1891 <137960726+asb1891@users.noreply.github.com>
2024-01-12 08:08:46 -0500

    Upcoming weather component logic improved

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, FlatList, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, FlatList, View, StatusBar, Image, ImageBackground } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
   const DATA = [
@@ -43,35 +43,62 @@ import { Feather } from '@expo/vector-icons'
 const Item = ( props ) => {
     const { dt_txt, min, max, condition } = props
     return (
-        <View>
+        <View style={styles.item}>
             <Feather name="sun" size={50} color={'white'} />
-            <Text>{dt_txt}</Text>
-            <Text>{min}</Text>
-            <Text>{max}</Text>
+            <Text style={styles.date}>{dt_txt}</Text>
+            <Text style={styles.temp}>{min}</Text>
+            <Text style={styles.temp}>{max}</Text>
         </View>
     )
 }
 const UpcomingWeather = () => {
     const renderItem = ({ item }) => (
-        <Item condition={item.weather[0].main} dt_txt={item.dt_txt} min={item.main.temp_min} max={item.main.temp_max} />
+        <Item condition={item.weather[0].main} 
+        dt_txt={item.dt_txt} min={item.main.temp_min}
+        max={item.main.temp_max} />
     )
 
 
   return (
-    <SafeAreaView styles={styles.container}>
-      <Text>Upcoming Weather</Text>
-      <FlatList
+    <SafeAreaView style={styles.container}>
+        <ImageBackground source={require('../../assets/cumulus-499176_1920.jpg')} style={styles.image}>
+        <Text>Upcoming Weather</Text>
+        <FlatList
         data={DATA}
         renderItem={renderItem}
         keyExtractor={item => item.dt_txt} 
      />
+     </ImageBackground>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1
-  }
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'royalblue'
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    alignContent: 'center',
+    borderWidth: 5, 
+    backgroundColor: 'pink',
+  },
+  temp: {
+    color: 'white',
+    fontSize: 20,
+  }, 
+  date: {
+    color: 'white',
+    fontSize: 15,
+  }, 
+  image: {
+  flex: 1,
+}
 })
 
 export default UpcomingWeather
